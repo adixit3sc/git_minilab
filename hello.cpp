eb05b361e8b43637628ae006e9dc3b6a1cc7e216
Arnav Dixit <adixit3@scu.edu>
2024-03-10 20:19:11 -0700

    final commit

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "myclass.h"
+
+using namespace std;
+
+int main () {
+  cout << "Hello World" << endl;
+
+  MyClass a(5);
+  int e = 2222;
+  cout << a.getX() << endl;
+}
---
+++ myclass.cpp
@@ -0,0 +1,15 @@
+#include "myclass.h"
+
+MyClass::MyClass() {
+}
+
+MyClass::MyClass(int x_) { 
+  x = x_; 
+}
+
+int MyClass::getX() { 
+  int y = 3;
+  int a = 2;
+  int x = 33;
+  return x; 
+}
